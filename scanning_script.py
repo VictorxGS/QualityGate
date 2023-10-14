d589f1207f67daf9436b0b1c584c3400609ba121
VictorxGS <victorxgs@gmail.com>
2023-10-13 23:53:28 -0300

    Update improvements scan code

--- scanning_script.py
+++ scanning_script.py
@@ -8,15 +8,44 @@
 
 # Comando de execução do arquivo: python .\scanning_script.py .\config.json 'palavra0' 'palavra1' --white_list 'palavra 2' 'palavra 3' 
 
+# TO DO:
+# Verificar se o retorno da func white list é necessário
+# Testar filtragem da funcao separate (problema de falta de cobertura)
+# Resolver entrada de parâmetros
+
 import argparse
+from colorama import init, Fore
+
+init()
 
 def scan_white_list(white_list, linha):
-    if(len(white_list) != 0): # tratar problema do len
+    if(len(white_list) != 0):
         for word in white_list:
             if word in linha:
                 linha = linha.replace(word, "")
-    return linha
+    return linha  #verficar se realmente é necessário retorno
 
+def scan_black_list(black_list, linha, i):
+    if(len(black_list) != 0):
+        for word in black_list:
+            if word in linha:
+                founds.append(f'{word} enconrada - linha {i}')
+                print(Fore.RED + f'ALERT: "{word}" encontrada - linha {i}', end="")
+                print(Fore.RESET)
+
+def separate_words(linha): #Testar essa funcao
+    indesejados = ['\n', '\t', ',', '"', ' ']
+    return linha.replace(indesejados, '').split(':')
+
+def resume(founds):
+    print('#### SCAN RESUME ####')
+    for found in founds:
+        print(F'>>>  {found}')
+
+
+white_list = ['false', 'metrics', 'OrgfalseId', 'DEV']
+black_list = ['false', 'metrics', 'OrgfalseId', 'DEV']
+founds=[]
 
 parser = argparse.ArgumentParser(description='Parser para criar argumentos recebidos pela cli.')
 
@@ -27,31 +56,21 @@ parser.add_argument('--black_list', type=str, nargs='+', help='Palavras proibida
 args = parser.parse_args()
 
 caminho_arquivo = args.caminho_do_arquivo
-white_list = args.white_list
-black_list = args.black_list
-
-found=[]
-
-print(f'BLACK-LIST: {black_list}')
-print(f'WHITE-LIST: {white_list}')
+#white_list = args.white_list
+#black_list = args.black_list
 
 try:
     with open(caminho_arquivo, 'r') as arquivo:
         for i, linha in enumerate (arquivo, 1):
+            
+            print(f' {i}: {linha}') # Imprime cada linha do arquivo scaneado
 
-            print(f' {i}: {linha}', end='')
+            linha = scan_white_list(white_list, linha) #SCAN WHITE-LIST verficar se realmente é necessário retorno
+            scan_black_list(black_list, linha, i) #SCAN BLACK-LIST
 
-            linha = scan_white_list(white_list, linha)
-
-            if(len(black_list) != 0):
-                for w in black_list:
-                    if w in linha:
-                        print(f'key: {w} encontrada na linha {i}')
-       
 except FileNotFoundError:
     print(f'O arquivo "{caminho_arquivo}" não foi encontrado.')
 except Exception as e:
-    print(f"Ocorreu um erro ao acessar o arquivo: {e}")
+    print(f'Ocorreu um erro ao acessar o arquivo: {e}')
 
-print(f'Caminho do arquivo: {caminho_arquivo}')
-
+resume(founds)
